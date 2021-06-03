0d702c821f3e8d28c289c9c06e43953cd60a44f1
Raid <naji.raid@gmail.com>
2021-06-02 22:21:01 -0400

    initial portfolio commit

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+// Namespace
+const portfolio = {};
+
+// Document Ready
+$(document).ready(function(){
+    portfolio.init();
+})
+
+// Init
+portfolio.init = () => {
+    AOS.init();
+    portfolio.introHeading();
+    portfolio.hamburger();
+}
+
+// Typed.js function in header intro 
+portfolio.introHeading = () => {
+    let options = {
+        strings: ["", "Front-end Developer", "Personal Finance Entusiast", "Cat lover"],
+        typeSpeed: 50,
+        loop: true,
+        backDelay: 2000
+    };
+
+    let typed = new Typed('.introduction', options);
+}
+
+portfolio.hamburger = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const checkbox = document.querySelector("#toggle");
+  // On click
+  checkbox.addEventListener("change", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, -> open/close menu
+  })
+}
